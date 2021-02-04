30c3f85bc62ab52d185b3388e11cf72821e6cfe3
Adeyemi juwon <samju7778@gmail.com>
2021-02-04 02:59:27 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import Header from './components/header';
+import Footer from './components/footer';
+import Section from './components/section';
+// import Exchangebody from './components/exchangeBody';
+import './components/Header.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Header />
+      <div className="container">
+        <div className="container1">
+          <h1 className="header">currency converter</h1>
+          <Section />
+          {/* <Exchangebody /> */}
+          <Footer />
+        </div>
+      </div>
+    </>
   );
 }
 
