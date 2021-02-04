3cb4cdfb9b36bd7756362c6d1a47ddc4d24b6196
Adeyemi juwon <samju7778@gmail.com>
2021-02-04 10:50:43 +0100

    deploy build

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 // import { FaLinkedin, FaFacebook, FaInstagram, FaTwitter } from 'react-icons/fa';
 import { social } from './data';
 
